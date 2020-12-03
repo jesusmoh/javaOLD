e8a803a74327c6cb86cfe52bc5314715c274334e
Jesús Manuel Ortíz Hidalgo <31394170+jesusPro89@users.noreply.github.com>
2020-12-03 17:19:32 -0300

    javaAnnotationInjectionCDI

---
+++ javaAnnotationInjectionCDI/src/main/java/core/MessageEJB.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import javax.ejb.Stateless;
+
+
+@Stateless
+public class MessageEJB {
+
+    public String sms()
+    {
+        return " -_- SMS";
+    }
+    
+}
---
+++ javaAnnotationInjectionCDI/src/main/java/core/SimpleJavaClassMessage.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+/**
+ *
+ * @author JOrtiz
+ */
+public class SimpleJavaClassMessage {
+
+    public String sms() {
+        return " *_* Hi ";
+    }
+}
