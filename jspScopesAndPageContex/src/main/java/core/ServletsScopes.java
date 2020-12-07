18a48e40ecc8c628ea76c26a1cdbe41f644b56c3
Jesús Manuel Ortíz Hidalgo <31394170+jesusPro89@users.noreply.github.com>
2020-12-07 14:21:38 -0300

    jspScopesAndPageContex

---
+++ jspScopesAndPageContex/src/main/java/core/ServletsScopes.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author JOrtiz
+ */
+@WebServlet
+public class ServletsScopes extends HttpServlet {
+
+private static final long serialVersionUID = 1L;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ServletsScopes</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ServletsScopes at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // processRequest(request, response);
+        ServletContext aplicacion = request.getServletContext();
+        aplicacion.setAttribute("varApp", "HelloApp");
+        HttpSession session = request.getSession(true);
+        session.setAttribute("varSession", "helloSession");
+        request.setAttribute("helloRequest", "helloRequest");
+        RequestDispatcher despachador = request.getRequestDispatcher("/scopes.jsp");
+        despachador.forward(request, response);
+    }
+
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
