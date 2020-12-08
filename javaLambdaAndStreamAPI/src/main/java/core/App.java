31305decf34483548f86ef0db0879bdb1d2c9c81
Jesús Manuel Ortíz Hidalgo <31394170+jesusPro89@users.noreply.github.com>
2020-12-08 17:24:28 -0300

    javaLambdaAndStreamAPI

--- javaLambdaAndStreamAPI/src/main/java/core/App.java
+++ javaLambdaAndStreamAPI/src/main/java/core/App.java
@@ -1,6 +1,7 @@
 package core;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
 /**
@@ -9,9 +10,9 @@ import java.util.*;
  */
 public class App 
 {
-    
-    
+
     public static void main( String[] args )
+            
     {
         System.out.println( "Hello World!" );
         Person p = new Person();
@@ -20,9 +21,31 @@ public class App
         Person pp = new Person();
         pp.setId(2);
         pp.setName("manuel");
+         Person ppp = new Person();
+        ppp.setId(3);
+        ppp.setName("enzo");
         
-        List<Person> listPerson =  Arrays.asList(p,pp);
-                
-        listPerson.forEach(pt->System.out.println(pt.toString()));
+        List<Person> listPerson =  Arrays.asList(p,pp,ppp);
+         
+       // <editor-fold defaultstate="collapsed" desc=" CASE 1 CICLE">
+           listPerson.forEach(pt->System.out.println("names "+pt.toString()));
+       // </editor-fold> 
+           
+       // <editor-fold defaultstate="collapsed" desc=" CASE 2 FILTER">
+           List<Person> listPersonFiltered= listPerson.stream().filter(pe-> pe.getId()>0).collect(Collectors.toList());
+           listPersonFiltered.forEach(fg -> System.out.println("filtered "+fg.getName()));
+       // </editor-fold> 
+       
+       // <editor-fold defaultstate="collapsed" desc=" CASE 3 MAP + BLOCK">
+           List<Integer> idList= listPerson.stream().map(p1e-> p1e.getId()*20).collect(Collectors.toList());
+           idList.forEach(intValue -> {System.out.println("map/transfor "+intValue);});
+       // </editor-fold> 
+         
+       // <editor-fold defaultstate="collapsed" desc=" CASE 4 COMPLEX FILTER + MAP">
+           List<Integer> idList2= listPerson.stream().map(p1e-> p1e.getId()*20).filter(per-> per>30).collect(Collectors.toList());
+           idList2.forEach(intValue -> {System.out.println("mapcomplex "+intValue);});
+       // </editor-fold>
     }
+    
+    
 }
