1563f71de6c3c61089c0cf060e4a1083a7e9a4bd
Jesús Manuel Ortíz Hidalgo <31394170+jesusPro89@users.noreply.github.com>
2020-12-10 10:30:16 -0300

    DemoWebSpringBoot

---
+++ InitializrSpringbootProject/src/main/java/core/DemoWebSpringBootApplication.java
@@ -0,0 +1,13 @@
+package core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoWebSpringBootApplication.class, args);
+	}
+
+}
