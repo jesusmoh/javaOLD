6746a4f763120e52c6349d7ad9f33b9e9597e43e
Jesús Manuel Ortíz Hidalgo <31394170+jesusPro89@users.noreply.github.com>
2021-01-15 08:49:31 -0300

    jsfOnSpringBoot

---
+++ jsfOnSpringBoot/src/main/java/core/JsfOnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsfOnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsfOnSpringBootApplication.class, args);
+	}
+
+}
