7b2151c2d15e29d2e54383e93fcd1d0497bfca16
Jesús Manuel Ortíz Hidalgo <31394170+jesusPro89@users.noreply.github.com>
2021-01-16 16:32:33 -0300

    jsfSecureApp

---
+++ jsfSecureApp/src/main/java/core/persistence/UserFacadeDAO.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package core.persistence;
+
+import core.model.entities.User;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author JOrtiz
+ */
+@Stateless
+public class UserFacadeDAO extends AbstractFacade<User> {
+
+    @PersistenceContext(unitName = "webAppSecureJSF_PU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public UserFacadeDAO() {
+        super(User.class);
+    }
+    
+    public User findUserByName(String name) {
+        User user = new User();
+        try {
+            Query query = em.createNativeQuery("SELECT * FROM app_users WHERE name='"+name+"';",User.class);
+            user =  (User) query.getSingleResult();
+
+        } catch (Exception ex) {
+            return  user;
+        }
+        return  user;
+    }
+    
+}
